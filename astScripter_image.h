a46e618be3e34009045d5c581b9da633a2eef3c4
Jabberwocky <wangy5236@gmail.com>
2025-02-28 21:35:20 +0800

    modified:   astScripter_image.h

--- astScripter_image.h
+++ astScripter_image.h
@@ -6,6 +6,15 @@ class astScripter_image
 {
 public:
     bool init();
+    enum
+    {
+        REFRESH_NONE_MODE = 0,
+        REFRESH_NORMAL_MODE = 1,
+        REFRESH_SAYA_MODE = 2,
+        REFRESH_SHADOW_MODE = 4,
+        REFRESH_TEXT_MODE = 8,
+        REFRESH_CURSOR_MODE = 16
+    };
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Texture *texture;
@@ -29,6 +38,19 @@ public:
     unsigned char *resize_buffer;
     size_t resize_buffer_size;
 
-    void loadImage(std::string file);
+    SDL_Surface *loadImage(std::string file);
+    SDL_Surface *createRectangleSurface(char *filename, bool *has_alpha, unsigned char *alpha = NULL);
+    SDL_Surface *createSurfaceFromFile(char *filename, bool *has_alpha, int *location);
+
+    int resizeSurface(SDL_Surface *src, SDL_Surface *dst);
+    void alphaBlend(SDL_Surface *mask_surface, int trans_mode, Uint32 mask_value = 255, SDL_Rect *clip = NULL,
+                    SDL_Surface *src1 = NULL, SDL_Surface *src2 = NULL, SDL_Surface *dst = NULL);
+    void alphaBlendText(SDL_Surface *dst_surface, SDL_Rect dst_rect,
+                        SDL_Surface *src_surface, SDL_Color &color, SDL_Rect *clip, bool rotate_flag);
+    void makeNegaSurface(SDL_Surface *surface, SDL_Rect &clip);
+    void makeMonochromeSurface(SDL_Surface *surface, SDL_Rect &clip);
+    void refreshSurface(SDL_Surface *surface, SDL_Rect *clip_src, int refresh_mode = REFRESH_NORMAL_MODE);
+    void refreshSprite(int sprite_no, bool active_flag, int cell_no, SDL_Rect *check_src_rect, SDL_Rect *check_dst_rect);
+    void createBackground();
 };
 #endif
